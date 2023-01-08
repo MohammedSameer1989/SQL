41ed46cc006cc569ac7cd2ab93b02d5b18966a38
MohammedSameer1989 <98795847+MohammedSameer1989@users.noreply.github.com>
2023-01-08 10:37:30 +0530

    Add files via upload

---
+++ Assignment 1 SQL.sql
@@ -0,0 +1,13 @@
+use [Practice Data]
+select * from ['Human Resource$']
+
+Select top 5 * from ['Human Resource$'] order by [Emp ID] desc
+
+select * from ['Human Resource$']
+
+select Distinct [Department]
+from ['Human Resource$']
+
+Select * from ['Human Resource$']
+Select [First Name]
+from ['Human Resource$'] as HR
