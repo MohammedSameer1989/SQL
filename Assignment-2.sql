a32b57166e1b2927b3dec4f3bc842ebd0cd53f0a
MohammedSameer1989 <98795847+MohammedSameer1989@users.noreply.github.com>
2023-01-09 17:16:06 +0530

    Add files via upload

---
+++ Assignment-2.sql
@@ -0,0 +1,12 @@
+use [Practice Data]
+
+select * from ['Human Resource$']
+
+select LEFT([First Name],3)
+from ['Human Resource$']
+
+select * from ['Human Resource$']
+
+select* from ['Human Resource$'].[DailyRate]
+Department, MAX([DailyRate]) from ['Human Resource$'] As HR Group By Department
+
